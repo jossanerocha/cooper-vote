5d5ab935c869287b9108347e5a2f943f6bc338eb
jossanerocha <jossane@gmail.com>
2021-06-18 23:02:04 -0300

    Criação do projeto

---
+++ src/main/java/com/jorocha/coopervote/CooperVoteApplication.java
@@ -0,0 +1,13 @@
+package com.jorocha.coopervote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorocha/coopervote/CooperVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorocha.coopervote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
